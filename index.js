3c1381d78727d5be3825d50d522554b79fca7d8c
Ubuntu <ubuntu@ip-172-31-15-178.us-west-2.compute.internal>
2014-05-15 14:57:28 +0000

    Updated2 index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 23!')
+  response.send('Hello World 234!')
 })
 
 app.listen(app.get('port'), function() {
