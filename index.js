b266ca28d58c001bde66a7d9b103f723da220420
Ubuntu <ubuntu@ip-172-31-15-178.us-west-2.compute.internal>
2014-05-16 01:39:10 +0000

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var text = fs.readFileSync('test.md','utf8')
+var text = fs.readFileSync('index.html','utf8')
 
 console.log (text);
 
